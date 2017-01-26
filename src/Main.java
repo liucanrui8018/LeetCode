cc72daf606ceb7c18f8af008e8c0c454dd7ce02d
Administrator <1163236013liu@gmail.com>
2017-01-26 17:56:58 +0800

    leetcode

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,4 @@
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-
 public class Main {
 	public static void main(String[] args) {
-		int[] num = { -1, 0, 1, -1, -1, 2 };
-		System.out.println(threeSum(num));
-	}
-
-	public static List<List<Integer>> threeSum(int[] num) {
-		Arrays.sort(num);
-		List<List<Integer>> res = new LinkedList<>();
-		for (int i = 0; i < num.length - 2; i++) {
-			if (i == 0 || (i > 0 && num[i] != num[i - 1])) {
-				int lo = i + 1, hi = num.length - 1, sum = 0 - num[i];
-				while (lo < hi) {
-					if (num[lo] + num[hi] == sum) {
-						res.add(Arrays.asList(num[i], num[lo], num[hi]));
-						while (lo < hi && num[lo] == num[lo + 1])
-							lo++;
-						while (lo < hi && num[hi] == num[hi - 1])
-							hi--;
-						lo++;
-						hi--;
-					} else if (num[lo] + num[hi] < sum)
-						lo++;
-					else
-						hi--;
-				}
-			}
-		}
-		return res;
 	}
 }
---
+++ src/twoSum.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class twoSum {
+	// Given an array of integers, return indices of the two numbers such that
+	// they add up to a specific target.
+	//
+	// You may assume that each input would have exactly one solution.
+	//
+	// Example:
+	// Given nums = [2, 7, 11, 15], target = 9,
+	//
+	// Because nums[0] + nums[1] = 2 + 7 = 9,
+	// return [0, 1].
+
+	public static int[] twoSum(int[] numbers, int target) {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < numbers.length; i++) {
+			if (map.containsKey(numbers[i])) {
+				return new int[] { map.get(numbers[i]) + 1, i + 1 };
+			} else {
+				map.put(target - numbers[i], i);
+			}
+		}
+		return new int[] { 0, 0 };
+	}
+}
