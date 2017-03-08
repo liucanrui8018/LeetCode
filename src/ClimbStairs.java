9dffc0c59fe4504e9b54ad0f0da92cafe04bbd5b
刘灿锐 <1163236013liu@gmail.com>
2017-03-08 14:32:57 +0800

    add more

---
+++ src/ClimbStairs.java
@@ -0,0 +1,27 @@
+/**
+ * Created by 刘灿锐 on 2017/3/7 0007.
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ *
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ *
+ * Note: Given n will be a positive integer.
+ */
+public class ClimbStairs {
+    public int climbStairs(int n) {
+        // base cases
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int one_step_before = 2;
+        int two_steps_before = 1;
+        int all_ways = 0;
+
+        for (int i = 2; i < n; i++) {
+            all_ways = one_step_before + two_steps_before;
+            two_steps_before = one_step_before;
+            one_step_before = all_ways;
+        }
+        return all_ways;
+    }
+}
---
+++ src/PlusOne.java
@@ -0,0 +1,27 @@
+/**
+ * Created by 刘灿锐 on 2017/3/7 0007.
+ * Given a non-negative integer represented as a non-empty array of digits, plus one to the integer.
+ *
+ * You may assume the integer do not contain any leading zero, except the number 0 itself.
+ *
+ * The digits are stored such that the most significant digit is at the head of the list.
+ */
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        int[] newNumber = new int[n + 1];
+        newNumber[0] = 1;
+
+        return newNumber;
+    }
+}
