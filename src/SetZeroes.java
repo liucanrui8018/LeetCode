255533ca5f07f0aa85fe2c24b53be96695b3a1ad
刘灿锐 <1163236013liu@gmail.com>
2017-03-11 19:03:46 +0800

    add more

---
+++ src/SetZeroes.java
@@ -0,0 +1,37 @@
+/**
+ * Created by 刘灿锐 on 2017/3/10 0010.
+ * Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
+ */
+public class SetZeroes {
+    public void setZeroes(int[][] matrix) {
+        boolean fr = false, fc = false;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    if (i == 0) fr = true;
+                    if (j == 0) fc = true;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (fr) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (fc) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+    }
+}
---
+++ src/SortColors.java
@@ -0,0 +1,35 @@
+/**
+ * Created by 刘灿锐 on 2017/3/11 0011.
+ * Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+ *
+ * Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+ *
+ * Note:
+ * You are not suppose to use the library's sort function for this problem.
+ *
+ * Follow up:
+ * A rather straight forward solution is a two-pass algorithm using counting sort.
+ * First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total number of 0's, then 1's and followed by 2's.
+ *
+ * Could you come up with an one-pass algorithm using only constant space?
+ */
+public class SortColors {
+    public void sortColors(int[] nums) {
+        // 1-pass
+        int p1 = 0, p2 = nums.length - 1, index = 0;
+        while (index <= p2) {
+            if (nums[index] == 0) {
+                nums[index] = nums[p1];
+                nums[p1] = 0;
+                p1++;
+            }
+            if (nums[index] == 2) {
+                nums[index] = nums[p2];
+                nums[p2] = 2;
+                p2--;
+                index--;
+            }
+            index++;
+        }
+    }
+}
