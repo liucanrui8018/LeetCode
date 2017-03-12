fe5f5875fd752facdeab53bfba6400a922b680d5
刘灿锐 <1163236013liu@gmail.com>
2017-03-12 22:24:03 +0800

    add more

--- src/RemoveDuplicates.java
+++ src/RemoveDuplicates.java
@@ -10,7 +10,6 @@
  * Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
  */
 public class RemoveDuplicates {
-
     public int removeDuplicates(int[] nums) {
         int i = nums.length > 0 ? 1 : 0;
         for (int n : nums)
